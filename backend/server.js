bb533b3fe940203a1f20b2542cd1d6d1a389b636
PranavOak12 <138028561+PranavOak12@users.noreply.github.com>
2026-02-18 16:42:13 +0530

    Remove localhost from allowed CORS origins

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,6 @@ const PORT = process.env.PORT || 3000;
 ======================= */
 
 const allowedOrigins = [
-  "http://localhost:5173",
   "https://todooo-7hup24pb6-pranavs-projects-53c10624.vercel.app",
 ];
 
